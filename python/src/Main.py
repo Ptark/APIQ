4c1b3e714f5c93788cae9620094013e66e2f2c24
christian <ueeyt@student.kit.edu>
2020-04-09 15:50:56 +0200

    pretty print

--- python/src/Main.py
+++ python/src/Main.py
@@ -1,11 +1,12 @@
-from python.src import Utility
+import pprint
+
 from python.src.environments import APIQ
 
 
 def main():
-    number_of_evaluations = 10000  # number of evaluations of an agent in an environment for stochastic purposes
+    number_of_evaluations = 100000  # number of evaluations of an agent in an environment for stochastic purposes
     scores = APIQ.calculate_apiq_scores(number_of_evaluations)
-    print(scores)
+    pprint.pprint(scores, sort_dicts=False)
 
 
 if __name__ == '__main__':
