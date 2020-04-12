393bb71bfc28eb5c80f48cd9f1526789c7ac6d06
christian <ueeyt@student.kit.edu>
2020-04-12 13:14:36 +0200

    Introduce floating precision scaling factor

--- python/src/Utility.py
+++ python/src/Utility.py
@@ -1,6 +1,8 @@
 import dis
 import time
+from typing import List, Callable
 
+from python.src.agents.Agent import Agent
 from python.src.agents.HandcraftedAgent import HandcraftedAgent
 from python.src.agents.RandomAgent import RandomAgent
 from python.src.environments.BiasedCoinFlip import BiasedCoinFlip
@@ -8,35 +10,37 @@ from python.src.environments.DoubleCoinFlip import DoubleCoinFlip
 from python.src.environments.Environment import Environment
 from python.src.environments.Slide import Slide
 
+floating_precision_factor = 3400
+randomness_complexity = 25
+
 
 def get_scaling_factor(environment: Environment, randomness: bool = False) -> float:
     """Calculate the scaling factor from the complexity of an environment"""
-    return 1 / pow(2, environment_complexity(environment, randomness))
+    return 1 / pow(2, environment_complexity(environment, randomness) / floating_precision_factor)
 
 
-def environment_complexity(environment: Environment, randomness: bool = False) -> int:
+def environment_complexity(environment: Environment, randomness: bool = False) -> float:
     """Estimate complexity of an environment depending on its main method and use of random"""
-    randomness_complexity = 336
     complexity = method_complexity(environment.calculate_percept)
     if randomness:
         complexity += randomness_complexity
     return complexity
 
 
-def method_complexity(method) -> int:
+def method_complexity(method: Callable) -> float:
     """Estimate complexity of a method by counting bytecode instructions"""
-    bits_per_instruction = 16
+    # bits per instruction = 16
     bytecode = dis.Bytecode(method).dis()
-    return len([line for line in bytecode.splitlines() if line]) * bits_per_instruction
+    return len([line for line in bytecode.splitlines() if line])
 
 
-def get_random_bit():
+def get_random_bit() -> int:
     """Returns a pseudorandom bit from a timestamp"""
     seed = time.time()
-    return pow(2, int(str(seed).replace('.', '')[-5:]))
+    return pow(2, int(str(seed).replace('.', '')[-5:])) % 3 % 2
 
 
-def agents():
+def agents() -> List[type(Agent)]:
     """Initialize and return a list of agents"""
     return [
         RandomAgent,
@@ -44,7 +48,7 @@ def agents():
     ]
 
 
-def environments():
+def environments() -> List[type(Environment)]:
     """Initialize and return a list of environments"""
     return [
         BiasedCoinFlip,
